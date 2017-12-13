ee3cc5897d87e0ed4a0ac035782f8d7d4aab7609
Lucas Fink <lucas.fink@RBE.sk.ca>
2017-12-13 14:15:20 -0600

    Initial work on CSS grid
    Todo: add some buttons above and below the numbers to complement key shortcuts.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-$(window).keypress(function (e) {
-    $("#instructions").html("")
-    if (e.which === 'p'.charCodeAt(0)) {
+$(window).keypress(function (key) {
+
+    if (key.which === 'p'.charCodeAt(0)) {
         $("#rscore").html(parseInt($("#rscore").html()) + 1);
-    } else if (e.which === 'l'.charCodeAt(0)) {
+    } else if (key.which === 'l'.charCodeAt(0)) {
         if (!parseInt($("#rscore").html()) == 0) {
             $("#rscore").html(parseInt($("#rscore").html()) - 1);
         }
-    } else if (e.which === 'q'.charCodeAt(0)) {
+    } else if (key.which === 'q'.charCodeAt(0)) {
         $("#lscore").html(parseInt($("#lscore").html()) + 1);
-    } else if (e.which === 'a'.charCodeAt(0)) {
+    } else if (key.which === 'a'.charCodeAt(0)) {
         if (!parseInt($("#lscore").html()) == 0) {
             $("#lscore").html(parseInt($("#lscore").html()) - 1);
         }

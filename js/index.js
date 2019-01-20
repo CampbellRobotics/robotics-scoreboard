7b71cdd3f21177ee9c750c302b98a4106b8c0b99
lf- <lf-@users.noreply.github.com>
2019-01-19 20:03:29 -0600

    wip, timer needs delayed start

--- js/index.js
+++ js/index.js
@@ -1,64 +1,123 @@
 'use strict';
+/*jshint globalstrict: true*/
 
 let useAudio = true;
+// 4 minutes
+let matchLen = 4 * 60;
 
-function doSeriesInt(synth, atTime, series) {
+function addSeriesToSynth(synth, atTime, series) {
+    if (!useAudio) { return; }
     let part = new Tone.Part(function (time, event) {
         synth.triggerAttackRelease(event.note, event.dur, time);
     }, series);
     part.start(atTime);
 }
 
-function setupAudioCues() {
-    let synth = new Tone.Synth().toMaster();
-    let doSeries = doSeriesInt.bind(null, synth);
-
-    
-    doSeries(0, [
-        {time: 0, note: 'F#4', dur: '8n'},
-        {time: 1, note: 'Bb4', dur: '16n'},
-        {time: 2, note: 'C#5', dur: '16n'},
-        {time: 3, note: 'F#5', dur: '4n'},
-    ]);
-
-    doSeries(64, [
-        {time: 0, note: 'G4', dur: '8n'},
-        {time: '4n', note: 'E4', dur: '16n'},
-    ]);
-
-    doSeries(124, [
-        {time: 0, note: 'G4', dur: '8n'},
-        {time: '4n', note: 'D4', dur: '16n'},
-    ]);
-
-
-    doSeries(184, [
-        {time: 0, note: 'G4', dur: '8n'},
-        {time: '4n', note: 'C4', dur: '16n'},
-    ]);
-
-
-    doSeries(244, [
-        {time: 0, note: 'C3', dur: '2n'},
-        {time: '16n', note: 'Eb3', dur: '2n'},
-        {time: '8n', note: 'F#3', dur: '2n'},
-    ]);
+function clamp(n, min, max) {
+    if (n < min) {
+        return min;
+    } else if (n > max) {
+        return max;
+    }
+    return n;
 }
 
-setupAudioCues();
+class Clock {
+    constructor(timerLength, clockElem, startBtn) {
+        this.timerLength = timerLength;
+        this.clockElem = clockElem;
+        this.startBtn = startBtn;
+        console.assert(this.timerLength > 0, 'got matchLenSeconds less than zero!');
+        this._setupAudioCues();
+    }
 
-function startAudio () {
-    stopAudio();
-    Tone.Transport.start('+0.1');
-};
+    startStop() {
+        console.log('Timer start/stop');
+        if (Tone.Transport.state === 'paused' || Tone.Transport.state === 'stopped') {
+            this.startBtn.html('Stop');
+            this.updateTimer();
+            Tone.Transport.start();
+        } else {
+            this.startBtn.html('Start');
+            this.pause();
+        }
+    }
 
-function stopAudio () {
-    Tone.Transport.stop();
-};
+    reset() {
+        this.stop();
+        Tone.Transport.seconds = 0;
+        this.updateTimer();
+    }
 
-function pauseAudio () {
-    Tone.Transport.pause();
-};
+    stop() {
+        Tone.Transport.stop();
+    }
+
+    pause() {
+        Tone.Transport.pause();
+    }
+
+    getRemainingMatchTime() {
+        const raw = (this.timerLength - Tone.Transport.getSecondsAtTime());
+        return clamp(raw, 0, matchLen);
+    }
+
+    updateTimer() {
+        const secs = this.getRemainingMatchTime();
+        const min = Math.floor(secs / 60);
+        const sec = Math.floor(secs % 60);
+        this.clockElem.html(`${min}:${sec.toString().padStart(2, '0')}`);
+    }
+
+    _setupAudioCues() {
+        let synth = new Tone.Synth().toMaster();
+        let addSeries = addSeriesToSynth.bind(null, synth);
+
+        const startSeries = [
+            { time: 0, note: 'F#4', dur: '8n' },
+            { time: 1, note: 'Bb4', dur: '16n' },
+            { time: 2, note: 'C#5', dur: '16n' },
+            { time: 3, note: 'F#5', dur: '4n' },
+        ];
+
+        // add one because we are counting from zero
+        const startSeriesTime = startSeries.reduce((accu, second) => Math.max(accu, second.time), 0) + 1;
+
+        const minuteSeries = [
+            [
+                { time: 0, note: 'G4', dur: '8n' },
+                { time: '4n', note: 'E4', dur: '16n' }
+            ],
+            [
+                { time: 0, note: 'G4', dur: '8n' },
+                { time: '4n', note: 'D4', dur: '16n' },
+            ],
+            [
+                { time: 0, note: 'G4', dur: '8n' },
+                { time: '4n', note: 'C4', dur: '16n' },
+            ],
+        ];
+
+        const endSeries = [
+            { time: 0, note: 'C3', dur: '2n' },
+            { time: '16n', note: 'Eb3', dur: '2n' },
+            { time: '8n', note: 'F#3', dur: '2n' },
+        ];
+
+        Tone.Transport.scheduleRepeat(this.updateTimer.bind(this), '0.1s', '0s', this.timerLength.toString() + 's');
+        addSeries(0, startSeries);
+
+        // one minute is subtracted because we want to play an endSeries on 0:00
+        // instead of a minuteSeries
+        for (let min = 1; min < Math.floor(this.timerLength / 60) - 1; ++min) {
+            addSeries(60 * min + startSeriesTime, minuteSeries[min % minuteSeries.length]);
+        }
+
+        addSeries(this.timerLength + startSeriesTime, endSeries);
+    }
+}
+
+const clock = new Clock(matchLen, $('#timer_time'), $('#timer_start'));
 
 var scores = {
     left: ko.observable(0),
@@ -67,7 +126,7 @@ var scores = {
 
 var viewModel = {
     scores: scores
-}
+};
 
 ko.applyBindings(viewModel);
 
@@ -97,59 +156,6 @@ $(window).keypress(function (key) {
     keyTuple[0](...keyTuple.slice(1));
 });
 
-
-class Timer {
-    constructor(totalTimeSec) {
-        this.totalTimeOrig = totalTimeSec * 1000;
-        this.totalTime = totalTimeSec * 1000;
-        this.startTime = null;
-        this.stopped = true;
-    }
-
-    start() {
-        this.startTime = performance.now();
-        this.stopped = false;
-    }
-
-    stop() {
-        this.totalTime -= (this.totalTime - this.currentTime);
-        this.stopped = true;
-    }
-
-    startStop() {
-        if (this.stopped) {
-            this.start();
-        } else {
-            this.stop();
-        }
-    }
-
-    reset() {
-        this.startTime = performance.now();
-        this.totalTime = this.totalTimeOrig;
-    }
-
-    updateElem(elem) {
-        const secs = this.currentTime / 1000;
-        const min = Math.floor(secs / 60);
-        const sec = Math.floor(secs % 60);
-        elem.html(`${min}:${sec.toString().padStart(2, '0')}`);
-    }
-
-    get currentTime() {
-        if (this.stopped) {
-            return this.totalTime;
-        }
-        let t = (this.totalTime - (performance.now() - this.startTime));
-        return t > 0 ? t : 0;
-    }
-
-    get currentTimeSec() {
-        return this.currentTime / 1000;
-    }
-}
-
-
 const directions = {
     plus: 1,
     minus: -1
@@ -157,7 +163,7 @@ const directions = {
 
 for (let scoreSide of $('.score_side')) {
     for (let btn of $(scoreSide).find('> .button')) {
-        $(btn).click(function (a) {
+        $(btn).click(function () {
             let direction;
             $(btn).attr('class').split(' ').forEach(function (cls) {
                 let tryDir;
@@ -173,51 +179,15 @@ for (let scoreSide of $('.score_side')) {
     }
 }
 
-const timer = new Timer(4 * 60);
-let interval = null;
-let updateTimer = Timer.prototype.updateElem.bind(timer, $('#timer_time'));
-let startStopTimer = Timer.prototype.startStop.bind(timer);
-updateTimer();
-
-function timerStartStop (btn) {
-    console.log('Timer start/stop');
-    if (timer.stopped) {
-        btn.html('Stop');
-        updateTimer();
-        interval = window.setInterval(updateTimer, 100);
-        if (timer.currentTimeSec === 4*60) {
-            // just got reset
-            Tone.Transport.schedule(startStopTimer, 4);
-        } else {
-            startStopTimer();
-        }
-        Tone.Transport.start();
-    } else {
-        btn.html('Start');
-        window.clearInterval(interval);
-        interval = null;
-        timer.startStop();
-        Tone.Transport.pause();
-    }
-}
-
-function resetRound (startBtn) {
+function resetRound() {
     console.log('Timer reset');
-    timer.stop();
-    timer.reset();
-    startBtn.html('Start');
-    window.clearInterval(interval);
-    interval = null;
-    updateTimer();
-    stopAudio();
-    Tone.Transport.cancel();
-    setupAudioCues();
+    clock.reset();
     clearScores();
 }
 
 for (let btn of $('#timer_start')) {
     btn = $(btn);
-    btn.click(timerStartStop.bind(Object, btn));
+    btn.click(clock.startStop.bind(clock));
     for (let resetBtn of $('#timer_reset')) {
         resetBtn = $(resetBtn);
         resetBtn.click(resetRound.bind(Object, btn));
@@ -229,6 +199,6 @@ const keys = {
     p: [changeScore, 'right', +1],
     a: [changeScore, 'left', -1],
     q: [changeScore, 'left', +1],
-    r: [resetRound, $('#timer_start')],
-    [' ']: [timerStartStop, $('#timer_start')],
+    r: [resetRound],
+    [' ']: [clock.startStop.bind(clock)],
 };
